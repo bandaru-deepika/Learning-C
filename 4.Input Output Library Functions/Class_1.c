2ac95af334b35645b9a671923983843c1d5bc443
bandaru-deepika <109798720+bandaru-deepika@users.noreply.github.com>
2023-02-09 17:49:17 +0530

    Add files via upload

---
+++ 4.Input Output Library Functions/Class_1.c
@@ -0,0 +1,50 @@
+/*
+Input Output Library Functions
+1. printf() and scanf()
+2. gets() and puts()
+3. getchar() and putchar()
+4. fgets() and fputs()
+5. fgetc() and fputc()
+
+printf() and scanf()
+- printf() is used to display the output on the screen.
+- scanf() is used to read the input from the keyboard.
+
+Syntax:
+printf("format string", arguments);
+scanf("format string", arguments);
+
+gets() and puts()
+- gets() is used to read a string from the keyboard.
+- puts() is used to display a string on the screen.
+
+Syntax:
+gets(string);
+puts(string);
+
+getchar() and putchar()
+- getchar() is used to read a character from the keyboard.
+- putchar() is used to display a character on the screen.
+
+Syntax:
+getchar();
+putchar(character);
+
+fgets() and fputs()
+- fgets() is used to read a string from the file.
+- fputs() is used to display a string on the screen.
+
+Syntax:
+fgets(string, size, file pointer);
+fputs(string, file pointer);
+
+fgetc() and fputc()
+- fgetc() is used to read a character from the file.
+- fputc() is used to display a character on the screen.
+
+Syntax:
+fgetc(file pointer);
+fputc(character, file pointer);
+
+
+*/
