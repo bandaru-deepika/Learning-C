998fdd38e97b5dd7f8d509619d03b78db7abc2bb
bandaru-deepika <109798720+bandaru-deepika@users.noreply.github.com>
2023-02-09 17:48:17 +0530

    Added Files

---
+++ 3.Data Types/Class_1.c
@@ -0,0 +1,26 @@
+/*
+Data modifers are used to modify the data types.
+There are two types of data modifiers in C:
+- Signed
+- Unsigned
+
+Signed:
+- The signed modifier is used to indicate that a variable can hold both positive and negative values.
+- The signed modifier is used by default.
+
+Syntax:
+signed <data type> <variable name> = <value>;
+
+Example:
+signed int a = 10;
+
+Unsigned:
+- The unsigned modifier is used to indicate that a variable can hold only positive values.
+
+Syntax:
+unsigned <data type> <variable name> = <value>;
+
+Example:
+unsigned int a = 10;
+
+*/
---
+++ 3.Data Types/Class_2.c
@@ -0,0 +1,31 @@
+/*
+Format Specifier
+%c	Character
+Eg: printf("%c", 'A');
+%d	Signed decimal integer
+Eg: printf("%d", 10);
+%e	Scientific notation (mantissa/exponent)
+e+value - 10 power + values
+e-value - 10 power - values
+Eg: printf("%e", 10.5);
+Eg: printf("%e", 10e5);
+%f	Floating point real number
+Eg: printf("%f", 10.5);
+%g	Use the shortest representation: %e or %f
+Eg: printf("%g", 10.5);
+%i	Signed decimal integer
+Eg: printf("%i", 10);
+%o	Unsigned octal
+Eg: printf("%o", 10);
+%s	String of characters
+Eg: printf("%s", "Hello World");
+%u	Unsigned decimal integer
+Eg: printf("%u", 10);
+%x	Unsigned hexadecimal integer
+Eg: printf("%x", 10);
+%X	Unsigned hexadecimal integer (capital letters)
+Eg: printf("%X", 10);
+%%	A % followed by another % character will write a single % to the screen
+Eg: printf("%%");
+
+*/
